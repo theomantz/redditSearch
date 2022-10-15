eeaf95929f550b536c2e29501e8c91f0150cf64c
theo m <theo@mantz.io>
2022-10-15 13:32:07 -0400

    Update

--- build/index.js
+++ build/index.js
@@ -4,5 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const getRedditData_1 = __importDefault(require("./getRedditData"));
-(0, getRedditData_1.default)("https://www.reddit.com/r/avesNYC_tix/new.json");
+(0, getRedditData_1.default)("https://www.reddit.com/r/avesNYC_tix/new.json", "tiki disco");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import main from "./getRedditData";
 
-main("https://www.reddit.com/r/avesNYC_tix/new.json");
+main("https://www.reddit.com/r/avesNYC_tix/new.json", "tiki disco");
